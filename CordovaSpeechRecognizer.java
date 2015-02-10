62847d993b4700180499bad890a8c3d1dd87d964
Matt Jewell <m@mattjewell.co.uk>
2015-02-10 11:49:13 +0000

    this

--- CordovaSpeechRecognizer.java
+++ CordovaSpeechRecognizer.java
@@ -112,7 +112,7 @@ public class CordovaSpeechRecognizer extends CordovaPlugin {
         mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                                          RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,
-                                         this.getPackageName());
+                                         cordova.getActivity().getPackageName());
 
 
         SpeechRecognitionListener listener = new SpeechRecognitionListener();
